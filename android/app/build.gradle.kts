1ee0524cc8cb25eedf1d8198c8dd2db9e837ec51
vinayak-10 <46519512+vinayak-10@users.noreply.github.com>
2026-06-24 16:21:57 +0530

    Update build.gradle.kts, explicit sdk version number given

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -45,8 +45,8 @@ android {
         applicationId = "com.example.anydb_flutter"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        minSdk = 24
+        targetSdk = 34
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
